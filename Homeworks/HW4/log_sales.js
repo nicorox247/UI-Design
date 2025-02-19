a39064008e66771530f5d13b051822d074ce8412
Nick Felix <nf2573@columbia.edu>
2025-02-18 22:37:26 -0500

    Almost done with HW. Working on drag/drop and error messages

---
+++ Homeworks/HW4/log_sales.js
@@ -0,0 +1,31 @@
+let clients = [
+    "Shake Shack",
+    "Toast",
+    "Computer Science Department",
+    "Teacher's College",
+    "Starbucks",
+    "Subsconsious",
+    "Flat Top",
+    "Joe's Coffee",
+    "Max Caffe",
+    "Nussbaum & Wu",
+    "Taco Bell",
+];
+
+let sales = [
+	{
+		"salesperson": "James D. Halpert",
+		"client": "Shake Shack",
+		"reams": 100
+	},
+	{
+		"salesperson": "Stanley Hudson",
+		"client": "Toast",
+		"reams": 400
+	},
+	{
+		"salesperson": "Michael G. Scott",
+		"client": "Computer Science Department",
+		"reams": 1000
+	},
+]
